453146ee1edf55cd27b2111cae48158aeb661e9e
Rayan <sx2hidden@gmail.com>
2025-06-29 21:46:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,6 @@ def analyze():
     })
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
